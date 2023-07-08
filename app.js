e20825e8699090a9ee332b8377dfd3141a841939
Andrew Edmonds <edmondsandrew9@gmail.com>
2023-07-07 23:28:22 -0400

    initial commit

--- app.js
+++ app.js
@@ -4,7 +4,37 @@ console.log(
 );
 
 // Exercise 1
+const firstName = "Andrew";
+let lastName = "Edmonds";
+var age = 33;
 
 // Exercise 2
+let language = "Javascript";
+let createdYear = 1995;
+let isCaseSensitive = true;
 
 // Exercise 3
+let price = 19.99;
+let isOnSale = false;
+let salePercentage = 15;
+let stock = 0;
+let inStock = false;
+let selectedSize = "M";
+
+//Exercise 5
+let title = "Name of the Wind";
+let author = "Patrick Rothfuss";
+let pageCount = 722;
+let bookmark = 456;
+let hasRead = true;
+
+//
+let fullName = firstName + " " + lastName;
+
+console.log(fullName);
+//bonus
+fullName = `${firstName} ${lastName}`;
+ let city = "Columbus";
+ let passTime = "boxing";
+ let myStory = "Hey there! my namr is ${fullName}, I live in ${city}, and my hobbies are ${myPasstime}, gaming, and artwork!"  "
+console.log(myStory); 
